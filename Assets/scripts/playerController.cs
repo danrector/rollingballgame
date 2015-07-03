20d6f57a8db618dcbb95ace78047342a5cfa8506
Daniel Rector <rector.danielk@gmail.com>
2015-07-03 17:20:46 -0400

    finished the game and built osx app and webplayer version

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -5,12 +5,18 @@ using System.Collections;
 public class playerController : MonoBehaviour {
 	
 	public float speed;
+	public Text countText;
+	public Text winText;
 	
 	private Rigidbody rb;
+	private int count;
 	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
 	}
 	
 	void FixedUpdate ()
@@ -22,4 +28,24 @@ public class playerController : MonoBehaviour {
 		
 		rb.AddForce (movement * speed);
 	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("pickup")) 
+		{
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText ()
+	{
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12) 
+		{
+			winText.text = "Winning";
+		}
+	}
+
 }
